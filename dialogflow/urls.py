a63080c4bb2d03f3f27dc2487dbd81afec613e85
jmpark1115 <jmpark1115@gmail.com>
2018-08-22 16:31:59 +0900

    init

---
+++ dialogflow/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'dialogflow'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('fulfillment/', views.fulfillment),
+    path('fulfillment', views.fulfillment),
+]

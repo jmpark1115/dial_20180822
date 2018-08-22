d489a2a61507954eb07cd8230f1d2ce3ca506da6
jmpark1115 <jmpark1115@gmail.com>
2018-08-22 17:10:31 +0900

    init

--- env.sh
+++ env.sh
@@ -1,2 +1,14 @@
-myenv.sh
-.idea
+#!/bin/sh
+
+# FIXME: dialogflow api v1
+export DIALOGFLOW_CLIENT_ACCESS_TOKEN="your token"
+export DIALOGFLOW_DEVELOPER_ACCESS_TOKEN="your token"
+export DIALOGFLOW_WEB_DEMO_URL="your url"
+export JUPYTER_NOTEBOOK_TOKEN="hellopython"  # Jupyter Login 암호
+
+# etc
+export LC_ALL="ko_KR.UTF-8"
+alias python="python3"
+alias pip="pip3"
+
+echo "loaded."
